da8c067e9f9e03e74c311a72987f8cf61a6db682
Guilherme Maciel <sncglm@gmail.com>
2024-09-28 01:04:32 -0300

    FEAT: Add Renda Fixa Service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,17 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    provideHttpClient(
+      withInterceptors([authInterceptor]),
+    )
+    
+  ]
 };
---
+++ src/app/pages/renda-fixa/services/renda-fixa.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+import { Observable } from 'rxjs';
+import { RendaFixa } from '../models/renda-fixa.types';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RendaFixaService {
+  private httpClient = inject(HttpClient);
+  private rendaFixaUrl = `${environment.andbankApi}/renda-fixa`
+
+  getAllRendaFixa(): Observable<RendaFixa>{
+    return this.httpClient.get<RendaFixa>(this.rendaFixaUrl);
+  }
+
+}
